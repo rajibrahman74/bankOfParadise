57bb43af0c3972b0b2f603a54a963b3fbcc373f0
rajibrahman74e <rajiburrahman74@gmail.com>
2023-02-13 18:51:41 +0600

    [ADD]-verified-email-password

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 // Add cllick event handler with submit button
-document.getElementById ('submit-btn').addEventListener ('click', function () {
+document.getElementById("submit-btn").addEventListener("click", function () {
+  // get the email address inside the email field
 
-    // 
-})
+  //   always remember if you want a input field value than use .value
+  const emailField = document.getElementById("user-email");
+  const getEmail = emailField.value;
 
+  // get the email address inside the email field
+  const passwordField = document.getElementById("user-password");
+  const getPassword = passwordField.value;
 
+  // verified email and password
+  if (getEmail === "rajiburrahman74@gmail.com" && getPassword === "rajib's") {
+    console.log("hello rajib suhana");
+  }
+});
